702e369e0108a9a89d0381ea2b399de7f6ff4417
Ed Angelis <ed4ngelis@gmail.com>
2024-10-16 10:17:21 -0300

    feat: database connection

---
+++ src/db.js
@@ -0,0 +1,19 @@
+import { MongoClient } from "mongodb";
+
+const connectionString =
+  process.env.DB_URI ||
+  "mongodb+srv://ed4ngelis:SZpg5yyAWAaaVPx0@cluster0.ku49w.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+const client = new MongoClient(connectionString);
+
+let conn;
+
+try {
+  conn = await client.connect();
+  console.log("Connected to the database");
+} catch (e) {
+  console.error(e);
+}
+let db = conn.db("open-food-facts");
+
+export default db;
