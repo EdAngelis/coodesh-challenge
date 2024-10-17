54723ebf4ee20144574ee031ab33b4a9b405bbca
Ed Angelis <ed4ngelis@gmail.com>
2024-10-17 09:40:44 -0300

    feat: function update documents to the database from a JSON zipped

---
+++ src/util/updateProductsFromGzFile.js
@@ -0,0 +1,29 @@
+import getNObjectsFromJson from "./getNObjectsFromJson.js";
+import deleteFiles from "./deleteFiles.js";
+import zlib from "zlib";
+import fs from "fs";
+
+function updateProductsFromGzFile(source, destination, db) {
+  const collection = db.collection("products");
+  const unzip = zlib.createGunzip();
+  const readStream = fs.createReadStream(source);
+  const writeStream = fs.createWriteStream(destination);
+
+  readStream
+    .pipe(unzip)
+    .pipe(writeStream)
+    .on("finish", async () => {
+      console.log(`${destination.split(".")[0]} File extracted successfully`);
+      const products = await getNObjectsFromJson(destination, 100);
+
+      console.log("Products Updated");
+
+      await collection.insertMany(products);
+      await deleteFiles(source);
+      await deleteFiles(destination);
+
+      return;
+    });
+}
+
+export default updateProductsFromGzFile;
