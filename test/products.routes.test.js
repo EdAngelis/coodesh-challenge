4ebac88fea65a3863cb97e3eb52bb78f84efd0fe
Ed Angelis <ed4ngelis@gmail.com>
2024-10-19 12:50:05 -0300

    test: first test to each route

--- test/products.routes.test.js
+++ test/products.routes.test.js
@@ -2,27 +2,34 @@ import { expect } from "chai";
 import sinon from "sinon";
 import request from "supertest";
 import app from "../src/app.js";
-import { MongoClient, ObjectId } from "mongodb";
+import repository, { update } from "../src/repository/product.repository.js";
+import { ObjectId } from "mongodb";
 
-describe("GET /users", () => {
-  let findStub, dbStub, collectionStub, clientStub;
+describe("GET PUT /products", () => {
+  let fetchStub;
 
-  before(() => {
-    clientStub = sinon.stub(MongoClient.prototype, "connect").resolves();
-    collectionStub = {
-      find: sinon.stub(),
-      findOne: sinon.stub(),
-      updateOne: sinon.stub(),
-    };
-    dbStub = sinon.stub(MongoClient.prototype, "db").returns({
-      collection: () => collectionStub,
-    });
+  beforeEach(() => {
+    fetchStub = sinon.stub(repository, "fetch");
   });
 
-  after(() => {
-    // Restore stubs to avoid side effects
-    clientStub.restore();
-    dbStub.restore();
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it("should return One Product", async () => {
+    const product = {
+      _id: new ObjectId(),
+      code: 123456,
+      url: "https://example.com",
+      imported_t: "2020-02-07T16:00:00Z",
+    };
+    fetchStub.resolves(product);
+
+    const res = await request(app)
+      .get(`/products/${product.code}`)
+      .set("x-api-key", "my-api-key");
+
+    expect(res.status).to.equal(200);
   });
 
   it("should return a list of Products", async () => {
@@ -34,11 +41,44 @@ describe("GET /users", () => {
         imported_t: "2020-02-07T16:00:00Z",
       },
     ];
-    collectionStub.find.resolves(products);
-    const res = await request(app).get("/products");
+    sinon.stub(repository, "fetchAll").resolves(products);
+    const res = await request(app)
+      .get("/products")
+      .set("x-api-key", "my-api-key");
+    expect(res.body.length).to.equal(1);
+  });
+
+  it("should update One Product", async () => {
+    const product = {
+      _id: new ObjectId(),
+      code: 123456,
+      url: "https://example.com",
+      imported_t: "2020-02-07T16:00:00Z",
+    };
+    sinon.stub(repository, "update").resolves(product);
+
+    const res = await request(app)
+      .put(`/products/${product.code}`, {
+        url: "https://example.com.br",
+      })
+      .set("x-api-key", "my-api-key");
 
     expect(res.status).to.equal(200);
-    // expect(res.body).to.be.an("array");
-    // expect(res.body).to.deep.equal(products);
+  });
+
+  it("should Soft Delete the product", async () => {
+    const product = {
+      _id: new ObjectId(),
+      code: 123456,
+      url: "https://example.com",
+      imported_t: "2020-02-07T16:00:00Z",
+      status: "published",
+    };
+    sinon.stub(repository, "softDelete").resolves(product);
+    const res = await request(app)
+      .put(`/products/delete/${product.code}`)
+      .set("x-api-key", "my-api-key");
+
+    expect(res.status).to.equal(200);
   });
 });
